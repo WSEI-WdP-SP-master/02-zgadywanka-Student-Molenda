ff47debaa885fc0c2f98fc2ae1930075fe0fc08f
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2021-01-17 11:54:59 +0100

    Funkcja Losuj

---
+++ Zgadywanka/GraProceduralnie/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace GraProceduralnie
+{
+    partial class Program
+    {
+        const string ZA_DUZO = "Za dużo";
+        const string ZA_MALO = "Za mało";
+        const string TRAFIONO = "Trafiono";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra w Za duzo za mało!");
+
+            #region 1. Koputer losuje
+            int wylosowana = Losuj(1, 100);
+
+#if DEBUG
+            Console.WriteLine(wylosowana);  // do usunięcia w ostatecznej wersji kodu
+#endif
+
+            Console.WriteLine("Wylosowałem liczbę od 1 do 100.\n Odgadnij ją!");
+            #endregion
+
+            //POWTARZAJ AŻ Do ZGADNIĘCIA
+
+            //bool trafiono = false;
+            do
+            {
+                #region 2. Człowiek proponuje
+                Console.Write("Podaj swoja propozycję: ");
+                string napis = Console.ReadLine();
+
+                int propozycja;
+                if (!int.TryParse(napis, out propozycja))
+                {
+                    Console.WriteLine("Nie podałeś poprawnej liczby.\n Spróbuj raz jeszcze.");
+                    continue;
+                }
+
+                Console.WriteLine($"Zaproponowałeś: {propozycja}");
+
+                #endregion
+
+                #region 3. Komputer ocenia propozycję
+                if (propozycja < wylosowana)
+                {
+                    Console.WriteLine(ZA_MALO);
+                }
+                else if (propozycja > wylosowana)
+                {
+                    Console.WriteLine(ZA_DUZO);
+                }
+                else
+                {
+                    Console.WriteLine(TRAFIONO);
+                    break; //trafiono = true;
+                }
+
+                #endregion
+            }
+            while (true); //while (!trafiono);
+                          //KONIEC POWTARZANIA
+
+            Console.WriteLine("Koniec gry");
+        }
+    
+
+
+
+    }
+}
---
+++ Zgadywanka/GraProceduralnie/ProgramPart2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraProceduralnie
+{
+    partial class Program
+    {
+
+        /// <summary>
+        /// Losuje liczbę całkowitą z podanego zakresu włącznie z granicami
+        /// </summary>
+        /// <remarks>
+        /// ...
+        /// </remarks>
+        /// <example>
+        /// 
+        /// </example>
+        /// <param name="min">dolne ograniczenie przdziału losowania, włącznie</param>
+        /// <param name="max">górne ograniczenie przedziału losowania, włącznie</param>
+        /// <exception cref="ArgumentException">jesli min > max</exception>
+        /// <returns>wylosowana liczba z zakresu min...max</returns>
+        static int Losuj(int min, int max)
+        {
+            var _ = new Random();
+            int x = _.Next(min, max + 1);
+            return x;
+        }
+
+        static int Losuj1(int min, int max) => (new Random()).Next(min, max + 1);
+
+        Func<int, int, int> Losuj3 =   (min, max) => (new Random()).Next(min, max+1); // z uzyciem lambda notacji
+
+        // ===========================
+
+
+        static int WczytajLiczbe(string komunikat = "Podaj liczbę: ")
+        {
+            Console.WriteLine(komunikat);
+            string napis = Console.ReadLine();
+            if(napis == "x" || napis == "X")
+            {
+                Console.WriteLine("Poddałeś się. Koniec programu");
+                Environment.Exit(0);
+            }
+
+            int wynik = int.Parse(napis);
+            return wynik;
+        }
+
+
+    }
+}
