7f4b347c956bc23785e2be43feb6201abf368559
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2021-01-17 14:15:23 +0100

    ver 01

---
+++ Zgadywanka/GraModel/Gra.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GraModel
+{
+    public class Gra
+    {
+        private readonly int liczbaOdgadywana;
+        public int Min { get; private set; }
+        public int Max { get; private set; }
+        // historia ruchów
+
+        // ctor's
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <exception cref="ArgumentException">jeśli min > max</exception>
+        public Gra(int min, int max)
+        {
+            this.Min = min;
+            Max = max;
+            liczbaOdgadywana = (new Random()).Next(min, max + 1);
+            // historia
+        }
+
+        public Gra(): this(1,100) {  }
+
+        // inne metody
+
+    }
+}
